3a2c7af508be6e1c3fc4821b86d1ad700ac797bb
Vit Brunner <vit.brunner@gmail.com>
2026-02-06 21:24:44 +0100

    Fix port, vite single file, visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import { visualizer } from 'rollup-plugin-visualizer';
 import elmPlugin from 'vite-plugin-elm';
 
 export default defineConfig({
-    plugins: [elmPlugin()],
-    server: { hmr: false },
+    plugins: [
+        elmPlugin(),
+        viteSingleFile(),
+        // visualizer({
+        //     open: true,
+        //     gzipSize: true,
+        //     filename: "stats.html",
+        // }),
+    ],
+    server: {
+        port: 8423,
+        hmr: false,
+    },
 });
