8e47290daac375cddb9a349e7fa702cd5aa63754
Vit Brunner <vit.brunner@gmail.com>
2026-03-12 08:14:36 +0100

    Test first

--- tests/AnalyzerTest.elm
+++ tests/AnalyzerTest.elm
@@ -3,31 +3,38 @@ module AnalyzerTest exposing (..)
 import Analyzer exposing (analyze)
 import Codec
 import Expect
-import Maze exposing (emptyMaze)
+import Maze as M
 import Test exposing (..)
 
 
 simplePath = "sz:3,1;st:0,0;end:2,0;mz:o0o0o0"
-        |> Codec.decode |> Maybe.withDefault emptyMaze
+        |> Codec.decode |> Maybe.withDefault M.emptyMaze
 
 loopMaze = "sz:3,3;st:0,0;end:2,0;mz:"
     ++ "o0o0o0"
     ++ "o0x o0"
     ++ "o0o0o0"
-        |> Codec.decode |> Maybe.withDefault emptyMaze
+        |> Codec.decode |> Maybe.withDefault M.emptyMaze
 
 isolatedMaze = "sz:3,3;st:0,0;end:2,2;mz:"
     ++ "x x o0"
     ++ "x x x "
     ++ "x o0x "
-        |> Codec.decode |> Maybe.withDefault emptyMaze
+        |> Codec.decode |> Maybe.withDefault M.emptyMaze
 
 bridgeMaze = "sz:3,1;st:0,0;end:2,0;mz:o1l1o1"
-        |> Codec.decode |> Maybe.withDefault emptyMaze
+        |> Codec.decode |> Maybe.withDefault M.emptyMaze
 
 stairsMaze = "sz:2,1;st:0,0;end:1,0;mz:o0s1"
-        |> Codec.decode |> Maybe.withDefault emptyMaze
+        |> Codec.decode |> Maybe.withDefault M.emptyMaze
 
+occludedMaze = "sz:6,5;st:3,4;end:4,4;mz:"
+    ++ "x S1o0o0o0o0"
+    ++ "o1o1o1o0o2o0"
+    ++ "o2o2o0o0o0o1"
+    ++ "o3z1o1x o3x "
+    ++ "x o2o3x x x "
+        |> Codec.decode |> Maybe.withDefault M.emptyMaze
 
 suite : Test
 suite =
@@ -56,5 +63,12 @@ suite =
             , test "Total Cells" <| \_ -> (analyze bridgeMaze).totalCells |> Expect.equal 4
             , test "Unreachable" <| \_ -> (analyze bridgeMaze).unreachable |> Expect.equal 1
             , test "Path through bridge" <| \_ -> (analyze bridgeMaze).shortestPathLength |> Expect.equal (Just 2)
+            ]
+        , describe "Occluded Maze"
+            [ test "Reachable" <| \_ -> (analyze occludedMaze).occluding |> Expect.equal
+                [ ( 3, 0 )
+                , ( 5, 1 )
+                , ( 5, 3 )
+                ]
             ]
         ]
